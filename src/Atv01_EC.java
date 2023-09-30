87d593870aa4aaeb6370b767d64a93a22381d846
Cristiano-Rodrigo <cristiano.rdasilva81@gmail.com>
2023-09-29 21:50:29 -0300

    atualizacao arq Java

--- src/Atv01_EC.java
+++ src/Atv01_EC.java
@@ -1,10 +1,10 @@
 import java.util.Scanner;
-
 public class Atv01_EC {
-
     public static void main(String[] args) {
-
         Scanner leitor = new Scanner(System.in);
+
+
+
 
         // final = const
         System.out.print("Digite a primeira nota: ");
